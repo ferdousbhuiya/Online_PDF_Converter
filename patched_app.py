acec03a4eb505d42ed469196280665215f24b592
Md Ferdouse Hossain Bhuiya <64379026+ferdousbhuiya@users.noreply.github.com>
2026-09-02 00:08:50 -0400

    Add robots.txt and sitemap.xml for search indexing

--- patched_app.py
+++ patched_app.py
@@ -1,4 +1,5 @@
 import app as original_app
+from flask import Response
 from compare_patch import handle_compare_pdf
 from converter_patch import handle_compress_pdf, handle_pdf_to_ppt
 from frontend_pages import make_home_view, make_tools_view, about_page, contact_page
@@ -42,5 +43,28 @@ original_app.app.view_functions['index'] = make_home_view(original_app)
 original_app.app.add_url_rule('/tools', endpoint='tools_page', view_func=make_tools_view(original_app))
 original_app.app.add_url_rule('/about', endpoint='about_page', view_func=about_page)
 original_app.app.add_url_rule('/contact', endpoint='contact_page', view_func=contact_page)
+
+# Search-engine discovery routes.
+# These explicit routes must be registered after importing the original app so
+# /robots.txt and /sitemap.xml are not handled by the application's generic route.
+def robots_txt():
+    body = "User-agent: *\nAllow: /\n\nSitemap: https://bhuiyapdf.ferdous.us/sitemap.xml\n"
+    return Response(body, mimetype='text/plain')
+
+
+def sitemap_xml():
+    body = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url><loc>https://bhuiyapdf.ferdous.us/</loc></url>
+  <url><loc>https://bhuiyapdf.ferdous.us/tools</loc></url>
+  <url><loc>https://bhuiyapdf.ferdous.us/about</loc></url>
+  <url><loc>https://bhuiyapdf.ferdous.us/contact</loc></url>
+</urlset>
+"""
+    return Response(body, mimetype='application/xml')
+
+
+original_app.app.add_url_rule('/robots.txt', endpoint='robots_txt', view_func=robots_txt)
+original_app.app.add_url_rule('/sitemap.xml', endpoint='sitemap_xml', view_func=sitemap_xml)
 
 app = original_app.app
